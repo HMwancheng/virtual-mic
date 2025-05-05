d6d3eba6af75d82757adabf5448dc5cd6f4d9e39
HMwancheng <91036325+HMwancheng@users.noreply.github.com>
2025-05-05 14:10:56 +0100

    Update virtual_mic.py

--- virtual_mic.py
+++ virtual_mic.py
@@ -6,9 +6,9 @@ from ctypes import wintypes
 class VirtualMic:
     def __init__(self):
         self.running = True
-        self._setup_windows_api()
+        self._setup_api()
 
-    def _setup_windows_api(self):
+    def _setup_api(self):
         """初始化Windows API"""
         self.user32 = ctypes.WinDLL('user32')
         self.shell32 = ctypes.WinDLL('shell32')
@@ -24,45 +24,35 @@ class VirtualMic:
                 ('szTip', wintypes.WCHAR * 64)
             ]
         
-        self.NOTIFYICONDATA = NOTIFYICONDATA
         self.nid = NOTIFYICONDATA()
         self.nid.cbSize = ctypes.sizeof(NOTIFYICONDATA)
-        self.nid.uFlags = 0x1  # 仅使用NIF_MESSAGE
+        self.nid.uFlags = 0x1  # NIF_MESSAGE
         self.nid.uCallbackMessage = 0x400 + 1
         self.nid.szTip = "Virtual Mic\0"
 
-    def _create_default_icon(self):
-        """创建系统默认图标"""
-        # 使用系统标准图标
-        ICON_INFO = (ctypes.c_ubyte * (32 * 32 * 4))()
-        return self.user32.CreateIconFromResource(
-            ctypes.byref(ICON_INFO), len(ICON_INFO), True, 0x00030000
-        )
+    def _audio_callback(self, indata, outdata):
+        """替代NumPy的纯Python音频转发"""
+        for i in range(len(outdata)):
+            outdata[i] = indata[i]
 
     def audio_loop(self):
-        """极简音频转发"""
+        """音频处理线程"""
         import sounddevice as sd
-        def callback(indata, outdata, *_):
-            outdata[:] = indata
-            
         with sd.Stream(
             channels=1,
-            callback=callback,
-            samplerate=44100,
-            blocksize=1024
+            dtype='float32',
+            callback=lambda i,o,f,t,s: self._audio_callback(i, o),
+            samplerate=44100
         ):
             while self.running:
                 sd.sleep(1000)
 
     def run(self):
-        """主控制逻辑"""
+        """主程序"""
         # 隐藏控制台
         self.user32.ShowWindow(
             ctypes.windll.kernel32.GetConsoleWindow(), 0
         )
-        
-        # 设置图标
-        self.nid.hIcon = self._create_default_icon()
         
         # 启动音频线程
         threading.Thread(target=self.audio_loop, daemon=True).start()
@@ -73,21 +63,16 @@ class VirtualMic:
         # 消息循环
         msg = wintypes.MSG()
         while self.user32.GetMessageW(ctypes.byref(msg), 0, 0, 0):
-            if msg.message == self.nid.uCallbackMessage:
-                if msg.lParam == 0x0205:  # 右键点击
-                    self.running = False
-                    self.user32.PostQuitMessage(0)
-            
             self.user32.TranslateMessage(ctypes.byref(msg))
             self.user32.DispatchMessageW(ctypes.byref(msg))
         
         # 清理
+        self.running = False
         self.shell32.Shell_NotifyIconW(0x2, ctypes.byref(self.nid))
 
 if __name__ == "__main__":
     if sys.platform != "win32":
-        ctypes.windll.user32.MessageBoxW(0, "仅支持Windows系统", "错误", 0x10)
+        ctypes.windll.user32.MessageBoxW(0, "仅支持Windows", "错误", 0x10)
         sys.exit(1)
     
-    vm = VirtualMic()
-    vm.run()
+    VirtualMic().run()
